8465c8098153584565e1b69bfb14e0471215a65b
Julien Catania <julien.catania@gmail.com>
2020-08-05 23:45:16 +0200

    adding writing file and get data from body for post method

--- src/main.ts
+++ src/main.ts
@@ -2,14 +2,21 @@ import {Application} from 'https://deno.land/x/abc@v1.0.2/mod.ts';
 
 const app = new Application();
 const port = 1337;
+const static_path = "src/static"
 
-app.static("/static", "src/static");
+app.static("/static", static_path);
 app
 .get("/", (c) => {
     return "Hello, Abc!";
 })
-.post("/", (c) => {
-    return c.json({msg:'ok!'});
+.post("/:hash", async (c) => {
+    const { data } = await c.body<any>();
+    console.log(data)
+    const { hash } = c.params as { hash: string };
+
+    Deno.writeTextFile(`${static_path}/plop.txt`, "hello world")
+
+    return c.json({hash});
 })
 .start({port: port});
 console.info(`Server listening on port ${port}`)
